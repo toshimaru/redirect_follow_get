c6fb6481020772953fd5e5ce905ae1463e29be1c
Toshimaru <me@toshimaru.net>
2016-12-16 01:17:21 +0900

    Implement redirect_follow_get

--- lib/redirect_follow_get.rb
+++ lib/redirect_follow_get.rb
@@ -1,5 +1,20 @@
-require "redirect_follow_get/version"
+require 'redirect_follow_get/version'
+require 'net/http'
 
 module RedirectFollowGet
-  # Your code goes here...
+  class TooManyRedirects < StandardError; end
+end
+
+def redirect_follow_get(url, limit: 10)
+  raise RedirectFollowGet::TooManyRedirects, 'too many HTTP redirects' if limit.zero?
+
+  case response = Net::HTTP.get_response(URI(url))
+  when Net::HTTPSuccess
+    response
+  when Net::HTTPRedirection
+    location = response['location']
+    redirect_follow_get(location, limit: limit - 1)
+  else
+    response
+  end
 end
