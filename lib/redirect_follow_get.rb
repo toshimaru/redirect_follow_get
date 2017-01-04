1504b1716bdef46941648b86d7e46275dd73b53e
toshimaru <me@toshimaru.net>
2017-01-04 18:57:35 +0900

    normalize Addressable URI

--- lib/redirect_follow_get.rb
+++ lib/redirect_follow_get.rb
@@ -5,12 +5,21 @@ require 'net/http'
 
 module RedirectFollowGet
   class TooManyRedirects < StandardError; end
+
+  def self.parse_url(url)
+    return URI.parse(url) if url.ascii_only?
+    Addressable::URI.parse(url).normalize
+  rescue URI::InvalidURIError
+    # if InvalidURIError is raised in URI.parse(),
+    # fallback to Addressable::URI.parse() method.
+    Addressable::URI.parse(url).normalize
+  end
 end
 
 def redirect_follow_get(url, limit: 10)
   raise RedirectFollowGet::TooManyRedirects, 'too many HTTP redirects' if limit.zero?
 
-  uri = url.ascii_only? ? URI.parse(url) : Addressable::URI.parse(url)
+  uri = RedirectFollowGet.parse_url(url)
 
   case response = Net::HTTP.get_response(uri)
   when Net::HTTPSuccess
