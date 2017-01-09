6f27cd3bdce379dbb8b3ff6d6b63f71a255ee270
Toshimaru <me@toshimaru.net>
2017-01-09 23:57:10 +0900

    WIP: Add tests

--- test/redirect_follow_get_test.rb
+++ test/redirect_follow_get_test.rb
@@ -5,9 +5,13 @@ class RedirectFollowGetTest < Minitest::Test
     refute_nil ::RedirectFollowGet::VERSION
   end
 
-  def test_return_200
-    get "/"
+  def test_return_200_after_redirect
+    r = redirect_follow_get("http://google.com/?q=test")
+    assert_equal "200", r.code
+  end
 
-    assert_equal "Hello", last_response.body
+  def test_return_200_after_redirect_with_non_ascii
+    r = redirect_follow_get("http://google.com/?q=テスト")
+    assert_equal "200", r.code
   end
 end
